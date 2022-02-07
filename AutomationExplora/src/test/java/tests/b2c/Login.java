6f82007e9282e98a64a8dd2bb961a81b31461e3a
mpaolino <m.paolino@nexsoft.it>
2022-02-07 17:04:48 +0100

    pkopik

--- AutomationExplora/src/test/java/tests/b2c/Login.java
+++ AutomationExplora/src/test/java/tests/b2c/Login.java
@@ -6,14 +6,17 @@ import org.testng.annotations.Test;
 import actions.b2c.LoginAction;
 import globalSetup.b2c.setupDriver;
 import globalSetup.b2c.startPage;
+import pages.b2c.HomePage;
 import wrappers.WebWrapper;
 
 public class Login extends setupDriver  {
 
 	@Test
-	public static void login() {
+	public static void login() throws InterruptedException {
 		startPage.startPage();
 		LoginAction.clickOnMyAccount();
+		Thread.sleep(5000);
+		WebWrapper.clickOn(HomePage.getLoginUsername());
 		LoginAction.insertLoginUsername();
 		LoginAction.insertLoginPassword();
 		
