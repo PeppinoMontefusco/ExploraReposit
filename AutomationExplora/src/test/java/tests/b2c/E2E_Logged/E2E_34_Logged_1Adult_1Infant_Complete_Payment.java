a50bb6717dec92bffd8496db5c46ca4810ac634f
Gmontefusco <66117607+Gmontefusco@users.noreply.github.com>
2022-05-12 10:06:31 +0200

    asdasd

---
+++ AutomationExplora/src/test/java/tests/b2c/E2E_Logged/E2E_34_Logged_1Adult_1Infant_Complete_Payment.java
@@ -0,0 +1,115 @@
+package tests.b2c.E2E_Logged;
+
+import java.awt.AWTException;
+import java.util.ArrayList;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import actions.b2c.TouchXAdyenAction;
+import actions.b2c.TouchXLoginAction;
+import actions.b2c.AdobeHomePageAction;
+import actions.b2c.AdobeSearchCruiseAction;
+import globalSetup.b2c.API;
+import globalSetup.b2c.Configuration;
+import globalSetup.b2c.ExternalFunction;
+import globalSetup.b2c.setupDriver;
+import globalSetup.b2c.startPage;
+import wrappers.Report;
+import wrappers.TestListener;
+import wrappers.TestManager;
+import wrappers.VersonixMethodsB2C;
+import wrappers.WebWrapper;
+
+@Listeners(TestListener.class)
+
+public class E2E_34_Logged_1Adult_1Infant_Complete_Payment extends setupDriver {
+	
+	@Test
+	public static void bookingFlow1adultCompletePaymentLogged() throws InterruptedException, AWTException, UnirestException {
+		test=TestManager.startTest("E2E_34", "E2E Logged: Scenario 1 Adult 1 Infant - Pay Total","E2E");
+		startPage.startPage();
+		Report.passStep("Open Homepage");
+		AdobeHomePageAction.clickOnBookAJourney();
+		Report.passStep("Click On Book a Journey");
+		AdobeSearchCruiseAction.clickOnDestination();
+		Report.passStep("Select One Destination");
+		AdobeSearchCruiseAction.clickOnSelectAndClose();
+		Report.passStep("Click On Select and Close");
+		AdobeSearchCruiseAction.clickOnGuest();
+		Report.passStep("Click On Guest");
+		AdobeSearchCruiseAction.decreaseAdult();
+		Report.passStep("Set 1 Adult");
+		AdobeSearchCruiseAction.increaseInfant();
+		Report.passStep("Set 1 Infant");
+		AdobeSearchCruiseAction.clickSearchCruise();
+		Report.passStep("Click On Search Cruise");
+		WebWrapper.waitForJavascript();
+		VersonixMethodsB2C.startVersonixPage();
+		TouchXLoginAction.loginBaseTouchX();
+		VersonixMethodsB2C.searchTagAndClick("width: 102px", "flt-clip");
+		Report.passStep("Click On Book");
+	    WebWrapper.waitForJavascript();
+	    VersonixMethodsB2C.searchTagNotClickableAndClick("width: 111px","flt-clip");
+	    Report.passStep("Click On Cabin Category");
+		WebWrapper.waitForJavascript();
+		VersonixMethodsB2C.searchTagAndClick("width: 106.8px", "flt-clip"); 
+		Report.passStep("Click On Cabin Subcategory");
+		WebWrapper.waitForJavascript();
+		VersonixMethodsB2C.searchTagAndClick("height: 48px", "flt-clip");
+		Report.passStep("Click On Continue");
+		WebWrapper.waitForJavascript();
+		ArrayList<String> datiInfant=VersonixMethodsB2C.addInfant(1);
+		VersonixMethodsB2C.searchTagNotClickableAndClick("width: 111.8px","flt-clip");
+		Report.passStep("Click On Confirm");
+		WebWrapper.waitForJavascript();
+		VersonixMethodsB2C.findSpanAndClick("PAY TOTAL");
+		Report.passStep("Click On Pay Total");
+		Thread.sleep(500);
+		VersonixMethodsB2C.clickOnCheckBox("25", "svg");
+		Report.passStep("Click On First Privacy Checkbox");
+		Thread.sleep(500);
+		VersonixMethodsB2C.clickOnCheckBox("25", "svg");
+		Report.passStep("Click On Second Privacy Checkbox");
+		VersonixMethodsB2C.searchTagNotClickableAndClick("rgb(10, 34, 64)","flt-clip");
+		Report.passStep("Click On Apply");
+		WebWrapper.waitForJavascript();
+		TouchXAdyenAction.setCardNumber(new Configuration().cardNumber());
+		Report.passStep("Insert Card Number");
+		TouchXAdyenAction.setExpiryDate(new Configuration().expireDate());
+		Report.passStep("Insert Expiry Date");
+		TouchXAdyenAction.setCVV(new Configuration().cvv());
+		Report.passStep("Insert Cvv");
+		TouchXAdyenAction.clickOnPayButton();
+		Report.passStep("Click On Pay");
+		Thread.sleep(3000);
+		driver.switchTo().defaultContent();
+		VersonixMethodsB2C.searchTagNotClickableAndClick("width: 86.8px","flt-clip");
+		Report.passStep("Click On Confirmation Pop Up");
+		WebWrapper.waitForJavascript();
+		VersonixMethodsB2C.clickOnLabel("Store");
+		WebWrapper.waitForJavascript();
+		VersonixMethodsB2C.clickOnLabel("OK");
+		WebWrapper.waitForJavascript();
+		
+		
+		String reservationInfo=VersonixMethodsB2C.getSummaryInformation("Booking");
+		String invoiceInfo=VersonixMethodsB2C.getSummaryInformation("Invoice").replace(",","");
+		String bookingNumber =reservationInfo.substring(10, 14);
+		VersonixMethodsB2C.verifyValue(reservationInfo, API.getCabinNumber(bookingNumber), "Cabin number");
+		VersonixMethodsB2C.verifyValue(reservationInfo, API.getStatusBooking(bookingNumber), "Status");
+		VersonixMethodsB2C.verifyValue(invoiceInfo, API.getAmountBooking(bookingNumber, "80"), "Amount Total");
+		VersonixMethodsB2C.verifyValue(invoiceInfo, API.getAmountBooking(bookingNumber, "70"), "Amount Due");
+		VersonixMethodsB2C.verifyValue(API.getAmountBooking(bookingNumber, "80"), API.getAmountSinglePaymentsBooking(bookingNumber), "Payment Amount");
+		
+		
+		
+		
+		
+
+		}
+
+
+}
